39301ff18e3e449b538c1df4aad2a9a3ad19a0cc
Florian Ranson <florian.ranson59@gmail.com>
2019-09-16 14:34:11 +0200

    preFinal branche

--- src/core/Core.java
+++ src/core/Core.java
@@ -3,5 +3,6 @@ package core;
 public class Core {
 	public static void main(String[] args) {
 		System.out.println("Parce que c'est notre projet");
+		System.out.println("Parce que c'est notre projet branche");
 	}
 }
