4cfa04056a60a4be9278f4b04808598b4743d6a4
virgil <virgil@DESKTOP-CANJ39M>
2019-09-21 00:57:56 +0100

    Création du package d'Tnterface Homme Machine et de sa classe principale RenderSystem

---
+++ src/ihm/RenderSystem.java
@@ -0,0 +1,16 @@
+package ihm;
+
+import java.util.List;
+
+import Entity.Entity;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RenderSystem {
+	Stage st;
+	Scene sc;
+	
+	public RenderSystem(int rayon, List<Entity> corps) {
+		
+	}
+}
