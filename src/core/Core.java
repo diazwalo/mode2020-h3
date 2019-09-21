2617336022b1a7f1faa136fb617d1d5f9cba38c9
virgil <virgil@DESKTOP-CANJ39M>
2019-09-21 20:05:36 +0100

    Création du systeme opérationelle

--- src/core/Core.java
+++ src/core/Core.java
@@ -1,10 +1,19 @@
 package core;
 
-import Position.Position;
+import ihm.RenderSystem;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Core {
+public class Core extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		RenderSystem rs = new RenderSystem(500, null);
+		Stage stageRs = rs.create();
+		stageRs.show();
+	}
+	
 	public static void main(String[] args) {
-		Position pos = new Position();
-		System.out.println(pos.toString());
+		Application.launch(args);
 	}
 }
--- src/ihm/RenderSystem.java
+++ src/ihm/RenderSystem.java
@@ -4,13 +4,35 @@ import java.util.List;
 
 import Entity.Entity;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class RenderSystem {
 	Stage st;
 	Scene sc;
+	Pane p;
+	final Circle shape;
+	int diametre;
 	
 	public RenderSystem(int rayon, List<Entity> corps) {
-		
+		this.diametre = rayon * 2;
+		shape = new Circle(diametre/2, rayon, rayon);
+		shape.setFill(Color.DARKSLATEGREY);
+		this.st = new Stage();
+	}
+	
+	public Stage create() {
+		p = new Pane();
+        p.setPrefSize(diametre, diametre);
+        p.getChildren().add(shape);
+        
+        sc = new Scene(p);
+        
+        st.setScene(sc);
+        st.setTitle("Système");
+        
+        return st;
 	}
 }
