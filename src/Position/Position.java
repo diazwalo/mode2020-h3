199b945369909c9d44b30ea5751437d52c4ac0ac
Clement <clement.dessingue@gmail.com>
2019-09-21 18:23:07 +0200

    Ajout de deux sections entity et position et creation d'une classe position.java

---
+++ src/Position/Position.java
@@ -0,0 +1,36 @@
+package Position;
+
+public class Position {
+	private double posx;
+	private double posy;
+	
+	public Position(double posx, double posy) {
+		this.posx = posx;
+		this.posy = posy;
+	}
+	
+	public Position() {
+		posx = 0.0;
+		posy = 0.0;
+	}
+	
+	public void setPosX(double posx) {
+		this.posx = posx;
+	}
+	
+	public void setPosY(double posy) {
+		this.posy = posy;
+	}
+	
+	public double getPosX() {
+		return posx;
+	}
+	
+	public double getPosY() {
+		return posy;
+	}
+	
+	public String toString() {
+		return "[ x : "+posx+", y : "+posy+" ]";
+	}
+}
