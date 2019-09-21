d7a0001b545cc7c836033c0039207e989191f407
Clement <clement.dessingue@gmail.com>
2019-09-21 12:13:05 +0200

    Si ça commit c'est que je peux coder sur mon pc portable

--- src/core/Core.java
+++ src/core/Core.java
@@ -4,5 +4,6 @@ public class Core {
 	public static void main(String[] args) {
 		System.out.println("Parce que c'est notre projet");
 		System.out.println("Enfin sur intelliJ");
+		System.out.println("Salam");
 	}
 }
