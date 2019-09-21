4ba03ea7e0b4a43e625d0b6737ac9b6ef70621d9
Clement <clement.dessingue@gmail.com>
2019-09-21 18:47:54 +0200

    Ajout classe Vecteur.java

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,11 +1,12 @@
 package Entity;
 
 import Position.Position;
+import Position.Vecteur;
 
 public abstract class Entity {
 	protected double masse;
 	protected double taille;
 	protected Position position;
-	
-	
+	protected Vecteur direction;
+	protected Vecteur vitesse;
 }
---
+++ src/Position/Vecteur.java
@@ -0,0 +1,31 @@
+package Position;
+
+public class Vecteur {
+	private Position posDepart;
+	private Position posArrivee;
+	
+	public Vecteur(Position posDepart, Position posArrivee) {
+		this.posDepart = posDepart;
+		this.posArrivee = posArrivee;
+	}
+
+	public Position getPosDepart() {
+		return posDepart;
+	}
+
+	public void setPosDepart(Position posDepart) {
+		this.posDepart = posDepart;
+	}
+
+	public Position getPosArrivee() {
+		return posArrivee;
+	}
+
+	public void setPosArrivee(Position posArrivee) {
+		this.posArrivee = posArrivee;
+	}
+	
+	public String toString() {
+		return "{ vX : "+posDepart.toString()+" , vY : "+posArrivee.toString()+" }";
+	}
+}
